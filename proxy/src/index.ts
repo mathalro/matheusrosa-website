9744718d3bf5ab49b0cea21b6911129effec8a4f
Matheus Rosa <matheus.ar11@hotmail.com>
2023-12-28 01:46:01 +0000

    Adding http to cors

--- proxy/src/index.ts
+++ proxy/src/index.ts
@@ -12,7 +12,7 @@ app.use(morgan("tiny"));
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
-const allowedOrigins = ['http://localhost:8080', 'http://localhost:3000', 'http://matheusrosa.com'];
+const allowedOrigins = ['http://localhost:8080', 'http://localhost:3000', 'http://matheusrosa.com', 'https://matheusrosa.com'];
 const options: cors.CorsOptions = {
   origin: allowedOrigins
 };
