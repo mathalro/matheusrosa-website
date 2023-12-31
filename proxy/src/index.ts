3332758efcb9b88a644f92d0d97a16849b5f4e40
Matheus Rosa <matheus.ar11@hotmail.com>
2023-12-31 19:51:53 +0000

    Making the origin more flexible for test purposes

--- proxy/src/index.ts
+++ proxy/src/index.ts
@@ -22,7 +22,7 @@ const options: cors.CorsOptions = {
 app.use(cors(options));
 app.use(cookieParser());
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", settings.ALLOW_ORIGIN);
+    res.header("Access-Control-Allow-Origin", req.headers.origin);
     res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Credentials", 'true');
