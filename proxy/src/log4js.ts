f44936dc3d76ab19fa521553d46a09bde73e7b19
Matheus Rosa <matheus.ar11@hotmail.com>
2024-02-18 09:38:43 +0000

    Adding docker compose volume to save logs in host machine

--- proxy/src/log4js.ts
+++ proxy/src/log4js.ts
@@ -5,7 +5,7 @@ console.log("Configuring log4j");
 log4js.configure({
     appenders: {
         console: { type: 'console', "layout": { "type": "pattern", "pattern": "%d - %c:[%p]: %m" } },
-        fileLog: { type: 'file', filename: '~/appdata/logs/application-api.log' }
+        fileLog: { type: 'file', filename: '/var/log/application/application-api.log' }
     },
     categories: {
         default: { appenders: ['console', 'fileLog'], level: 'info' },
